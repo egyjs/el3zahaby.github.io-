4cd3b711f948f567fb40e8ff4bce244d9c35fa2a
Abdulrahman elzahaby <el3zahaby@gmail.com>
2018-02-04 22:32:43 +0300

    Add files via upload

--- assets/js/init.js
+++ assets/js/init.js
@@ -5,3 +5,19 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+        $(document).ready(function() {
+            var frame = $('iframe');
+            frame.addClass('card');
+            frame.css({
+                "width": "100%",
+                "height": "450",
+                "border": "0"
+            })
+
+            frame.each(function() {
+                var $this = $(this);
+                var _href = $this.attr("src");
+                $this.attr("src", "https://mozilla.github.io/pdf.js/web/viewer.html?file=" + _href);
+            });
+        });
