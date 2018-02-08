d95b7a894f7e24a6cdf23d8c9442f4b4849a3857
Abdulrahman elzahaby <el3zahaby@gmail.com>
2018-02-08 17:21:43 +0300

    Update init.js

--- assets/js/init.js
+++ assets/js/init.js
@@ -6,24 +6,24 @@
   }); // end of document ready
 })(jQuery); // end of jQuery name space
 
-        $(document).ready(function() {
-            var frame = $('iframe');
-            var Gogl = $('iframe[data-url="1"]');
-            frame.addClass('card');
-            frame.css({
-                "width": "100%",
-                "height": "450",
-                "border": "0"
-            })
+//         $(document).ready(function() {
+//             var frame = $('iframe');
+//             var Gogl = $('iframe[data-url="1"]');
+//             frame.addClass('card');
+//             frame.css({
+//                 "width": "100%",
+//                 "height": "450",
+//                 "border": "0"
+//             })
 
-            frame.not('[data-url="1"]').each(function() {
-                var $this = $(this);
-                var _href = $this.attr("src");
-                $this.attr("src", "https://mozilla.github.io/pdf.js/web/viewer.html?file=" + _href);
-            });
-            Gogl.each(function() {
-                var $this = $(this);
-                var _href = $this.attr("src");
-                $this.attr("src", "https://docs.google.com/viewer?url=" + _href + "&embedded=true");
-            });
-        });
+// //             frame.not('[data-url="1"]').each(function() {
+// //                 var $this = $(this);
+// //                 var _href = $this.attr("src");
+// //                 $this.attr("src", "https://mozilla.github.io/pdf.js/web/viewer.html?file=" + _href);
+// //             });
+//             Gogl.each(function() {
+//                 var $this = $(this);
+//                 var _href = $this.attr("src");
+//                 $this.attr("src", "https://docs.google.com/viewer?url=" + _href + "&embedded=true");
+//             });
+//         });
