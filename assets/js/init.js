97fd19c808a90598c1e3f52db8046d311202cab0
el3zahaby <el3zahaby@gmail.com>
2018-02-08 19:31:15 +0300

    style pdf

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,23 +1,26 @@
-(function($){
-  $(function(){
+(function ($) {
+    $(function () {
 
-    $('.button-collapse').sideNav();
+        $('.button-collapse').sideNav();
 
-  }); // end of document ready
+    }); // end of document ready
 })(jQuery); // end of jQuery name space
 
-        $(document).ready(function() {
-            var Gogl = $('div.url');
-            $(Gogl).replaceWith('<iframe src="' + $('div.url').html()  +'"></iframe>');
+$(document).ready(function () {
+    var Gogl = $('div.url');
+    $(Gogl).replaceWith('<iframe src="https://mozilla.github.io/pdf.js/web/viewer.html?file=https://el3zahaby.github.io/' + $('div.url').html() + '"></iframe>');
 
-            var frame = $('iframe');
-            frame.addClass('card');
-            frame.css({
-                "width": "100%",
-                "height": "450",
-                "border": "0"
-            }); 
+    var frame = $('iframe');
+    frame.addClass('card');
+    frame.css({
+        "width": "100%",
+        "height": "450",
+        "border": "0",
+        "position": "absolute",
+        "right": "0",
+        "left": "0"
+    });
 
 
-            
-        });
+
+});
