3530facdce960b467785b3f29115e748fc42a540
el3zahaby <el3zahaby@gmail.com>
2018-02-10 18:21:10 +0300

    test

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,7 +1,12 @@
 (function ($) {
     $(function () {
 
-        $('.button-collapse').sideNav();
+        $('.button-collapse').sideNav({
+      menuWidth: 300, // Default is 300
+      edge: 'right', // Choose the horizontal origin
+
+    }
+  );
 
     }); // end of document ready
 })(jQuery); // end of jQuery name space
