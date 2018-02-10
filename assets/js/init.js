f77638db84b320fb466e85ee41a85ecdf42baa6a
el3zahaby <el3zahaby@gmail.com>
2018-02-10 18:56:11 +0300

    left

--- assets/js/init.js
+++ assets/js/init.js
@@ -3,7 +3,7 @@
 
         $('.button-collapse').sideNav({
       menuWidth: 300, // Default is 300
-      edge: 'right', // Choose the horizontal origin
+      edge: 'left', // Choose the horizontal origin
 
     }
   );
