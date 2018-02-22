24b7831ab6856340cd756a99a25213a71c3463eb
el3zahaby <el3zahaby@gmail.com>
2018-02-22 19:54:15 +0300

    update file

--- assets/js/init.js
+++ assets/js/init.js
@@ -6,6 +6,9 @@
     }); // end of document ready
 })(jQuery); // end of jQuery name space
 
+function replaceThis(t){
+    return t.replaceWith('<iframe src="https://mozilla.github.io/pdf.js/web/viewer.html?file=https://el3zahaby.github.io/' + $(this).html() + '"></iframe>');
+}
 
 $(document).ready(function () {
     var PDFjs = $('div.url:first'),
@@ -14,8 +17,8 @@ $(document).ready(function () {
 //    $(PDFjs).first().replaceWith('<iframe src="https://mozilla.github.io/pdf.js/web/viewer.html?file=https://el3zahaby.github.io/' + $(this).html() + '"></iframe>');
     $(btnMore).replaceWith(function(){
          var txt  = $(this).html(),
-             func = '$(this).replaceWith(\'<iframe src="https://mozilla.github.io/pdf.js/web/viewer.html?file=https://el3zahaby.github.io/\' + $(\'div.url\').html() + \'"></iframe>\')';
-        return '<a onclick="'+func+'" class="waves-effect waves-light btn green darken-1" data-href="https://mozilla.github.io/pdf.js/web/viewer.html?file=https://el3zahaby.github.io/' + txt + '">الجزء التالي :</a>'
+
+        return '<a onclick="replaceThis($(this));" class="waves-effect waves-light btn green darken-1" data-href="https://mozilla.github.io/pdf.js/web/viewer.html?file=https://el3zahaby.github.io/' + txt + '">الجزء التالي :</a>'
     });
     var frame = $('iframe');
     frame.addClass('card');
